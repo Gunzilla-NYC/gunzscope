2f347bbd8d65b0bdd9d8fa91d416108526fb1278
cryptohaki <cryptohaki@protonmail.com>
2026-03-07 22:42:41 -0500

    cache: add revalidate to /credits

--- app/credits/page.tsx
+++ app/credits/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
 
+export const revalidate = 86400;
+
 export const metadata: Metadata = {
   title: 'Credits',
   description: 'The people and projects that make GUNZscope possible.',
